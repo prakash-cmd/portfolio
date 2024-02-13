3d80cd0f7e99924880315e17bb0bf5a475de56bc
Prakash Kumar <prakashkumarchaudhary02@gmail.com>
2024-02-14 01:12:06 +0530

    added complete about section

---
+++ src/components/about/Info.jsx
@@ -0,0 +1,32 @@
+import React from 'react';
+import { SiLeetcode } from "react-icons/si";
+import { FaUserGraduate, FaHome } from "react-icons/fa";
+import {HiOutlineArrowSmUp} from "react-icons/hi";
+
+
+const Info = () => {
+  return (
+    <div className="about__info grid">
+        <div className="about__box">
+            <FaUserGraduate className="about__icon" />
+            <h3 className="about__title">Amity</h3>
+            <span className="about__subtitle">2023 Graduate</span>
+        </div>
+        <div className="about__box">
+            <FaHome className="about__icon" />
+            <h3 className="about__title">Noida</h3>
+            <span className="about__subtitle">Uttar Pradesh</span>
+        </div>
+        <div className="about__box">
+            <SiLeetcode className="about__icon"/>
+            <h3 className="about__title">Leetcode</h3>
+            <span className="about__subtitle">
+                <a href="https://leetcode.com/prakash-cmd/" className="about__leetcode" target={"_blank"}>
+                    prakash-cmd</a>
+            </span>
+        </div>
+    </div>
+  );
+}
+
+export default Info;
