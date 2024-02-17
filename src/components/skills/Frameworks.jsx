a818e43f5ef3c8dcfd58f20b33f2ca00fbe01c0d
Prakash Kumar <prakashkumarchaudhary02@gmail.com>
2024-02-18 00:28:50 +0530

    added frameworks component for skills section

---
+++ src/components/skills/Frameworks.jsx
@@ -0,0 +1,60 @@
+import React from "react";
+import { HiCheckBadge } from "react-icons/hi2";
+
+const Frameworks = () => {
+    return (
+        <div className="skills__content">
+            <h3 className="skills__title">Technologies/Frameworks</h3>
+            <div className="skills__box">
+                <div className="skills__group">
+                    <div className="skills__data">
+                        <HiCheckBadge />
+                        <div>
+                            <h3 className="skills__name">Node.js</h3>
+                            <span className="skills__level">Intermediate</span>
+                        </div>
+                    </div>
+                    <div className="skills__data">
+                        <HiCheckBadge />
+                        <div>
+                            <h3 className="skills__name">Express.js</h3>
+                            <span className="skills__level">Intermediate</span>
+                        </div>
+                    </div>
+                    <div className="skills__data">
+                        <HiCheckBadge />
+                        <div>
+                            <h3 className="skills__name">DynamoDB</h3>
+                            <span className="skills__level">Intermediate</span>
+                        </div>
+                    </div>
+                </div>
+                <div className="skills__group">
+                    <div className="skills__data">
+                        <HiCheckBadge />
+                        <div>
+                            <h3 className="skills__name">iOS Development</h3>
+                            <span className="skills__level">Intermediate</span>
+                        </div>
+                    </div>
+                    <div className="skills__data">
+                        <HiCheckBadge />
+                        <div>
+                            <h3 className="skills__name">MySQL</h3>
+                            <span className="skills__level">Basic</span>
+                        </div>
+                    </div>
+                    <div className="skills__data">
+                        <HiCheckBadge />
+                        <div>
+                            <h3 className="skills__name">Docker</h3>
+                            <span className="skills__level">Basic</span>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default Frameworks;
