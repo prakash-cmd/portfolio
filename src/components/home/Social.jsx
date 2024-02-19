a245c116eec2df381300d24af79f61bea0d66527
Prakash Kumar <prakashkumarchaudhary02@gmail.com>
2024-02-19 16:20:24 +0530

    added leetcode profile into social

--- src/components/home/Social.jsx
+++ src/components/home/Social.jsx
@@ -1,20 +1,24 @@
 import React from "react";
 import { FiTwitter, FiGithub, FiLinkedin} from "react-icons/fi";
+import { SiLeetcode } from "react-icons/si";
 
 const Social = () => {
     return (
         <div className="home__social">
             <a href="https://twitter.com/chaudhury_19" className="home__social-icon" target="_blank">
-                <FiTwitter />
+                <FiTwitter/>
+            </a>
+            <a href="https://leetcode.com/nagato19/" className="home__social-icon" target="_blank">
+                <SiLeetcode/>
             </a>
             <a href="https://github.com/prakash-cmd" className="home__social-icon" target="_blank">
-                <FiGithub />
+                <FiGithub/>
             </a>
             <a href="https://www.linkedin.com/in/prakash-cmd/" className="home__social-icon" target="_blank">
-                <FiLinkedin />
+                <FiLinkedin/>
             </a>
-        </div> 
-    ); 
+        </div>
+    );
 }
 
 export default Social;
